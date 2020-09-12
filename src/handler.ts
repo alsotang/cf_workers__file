2791336c50332b2ce9ffac2aa05dd81ff3065230
alsotang <alsotang@gmail.com>
2020-09-12 16:38:51 +0800

    cache MAX_CHUNK

--- src/handler.ts
+++ src/handler.ts
@@ -2,7 +2,7 @@ import filesizeParser from 'filesize-parser'
 
 // per chunk size should not reach worker's memory limit
 // https://developers.cloudflare.com/workers/platform/limits
-const MAX_CHUNK_SIZE = 1024 * 1024 * 50; // 50MB
+const MAX_CHUNK_SIZE = 1024 * 1024 * 10; // 10MB
 
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url);
@@ -14,12 +14,17 @@ export async function handleRequest(request: Request): Promise<Response> {
 
   // return the readable first, then write to it
   setTimeout(async () => {
+    const MAX_CHUNK = new Uint8Array(MAX_CHUNK_SIZE)
     const writer = writable.getWriter()
 
     // use stream to keep memory usage small enough
     while (sendedSize < desiredSize) {
       const chunkSize = Math.min(desiredSize - sendedSize, MAX_CHUNK_SIZE);
-      await writer.write(new Uint8Array(chunkSize))
+      if (chunkSize === MAX_CHUNK_SIZE) {
+        await writer.write(MAX_CHUNK)
+      } else {
+        await writer.write(new Uint8Array(chunkSize))
+      }
       sendedSize += chunkSize;
     }
 
