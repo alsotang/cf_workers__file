e9630aa3178990b5699ee9f6d7afd92f9b641c96
alsotang <alsotang@gmail.com>
2020-09-12 14:32:40 +0800

    first version

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,36 @@
+// per chunk size should not reach worker's memory limit
+// https://developers.cloudflare.com/workers/platform/limits
+const MAX_CHUNK_SIZE = 1024 * 1024 * 50; // 50MB
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const url = new URL(request.url);
+
+  const desiredSize = getDesiredSize(url.pathname);
+  let sendedSize = 0;
+
+  let { readable, writable } = new TransformStream()
+
+  // return the readable first, then write to it
+  setTimeout(async () => {
+    const writer = writable.getWriter()
+
+    // use stream to keep memory usage small enough
+    while (sendedSize < desiredSize) {
+      const chunkSize = Math.min(desiredSize - sendedSize, MAX_CHUNK_SIZE);
+      await writer.write(new Uint8Array(chunkSize))
+      sendedSize += chunkSize;
+    }
+
+    writer.close()
+  }, 0);
+
+  return new Response(readable, {
+    headers: {
+      'Content-Disposition': 'attachment; filename="file.bin"'
+    }
+  })
+}
+
+function getDesiredSize(pathname: string) {
+  return Number(pathname.slice(1));
 }
