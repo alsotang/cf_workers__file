776595d355fab813f19ec561000bdda151ba9275
alsotang <alsotang@gmail.com>
2020-09-12 18:49:35 +0800

    redirect index to github repo

--- src/handler.ts
+++ src/handler.ts
@@ -7,6 +7,10 @@ const MAX_CHUNK_SIZE = 1024 * 1024 * 10; // 10MB
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url);
 
+  if (url.pathname === '/') {
+    return Response.redirect('https://github.com/alsotang/cf_workers__file', 302)
+  }
+
   const desiredSize = getDesiredSize(url.pathname);
   let sendedSize = 0;
 
